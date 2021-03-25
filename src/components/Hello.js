9fab79e1995fa3c5fb2625774ee8a30b99c6fd6f
Jerson Janke <jersonjanke@gmail.com>
2021-03-25 18:01:14 +0000

    Events

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Hello = (props) => {
-  if (props.name) {
-    return <h1>Hello, {props.name}</h1>;
-  } else {
-    return <span>Hey, stranger</span>;
-  }
+    if (props.name) {
+        return <h1>Hello, {props.name}</h1>;
+    } else {
+        return <span>Hey, stranger</span>;
+    }
 };
 
 export default Hello;
